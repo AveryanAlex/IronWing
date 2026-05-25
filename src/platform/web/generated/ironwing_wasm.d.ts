45c700c4b0836169037af1345b3257f0427bb058
AveryanAlex <alex@averyan.ru>
2026-05-25 21:54:59 +0300

    chore: update ironwing_wasm.d.ts

--- src/platform/web/generated/ironwing_wasm.d.ts
+++ src/platform/web/generated/ironwing_wasm.d.ts
@@ -97,7 +97,16 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
     readonly memory: WebAssembly.Memory;
+    readonly __wbg_wasmbytebridge_free: (a: number, b: number) => void;
+    readonly wasmbytebridge_close: (a: number) => void;
+    readonly wasmbytebridge_isClosed: (a: number) => number;
+    readonly wasmbytebridge_nextOutbound: (a: number) => any;
+    readonly wasmbytebridge_pushInbound: (a: number, b: any) => any;
     readonly __wbg_ironwingwasmruntime_free: (a: number, b: number) => void;
+    readonly availableMessageRates: () => [number, number, number];
+    readonly firmwareBootloaderCatalogTargetsFromManifest: (a: number, b: number, c: number, d: number) => [number, number, number];
+    readonly firmwareCatalogEntriesFromManifest: (a: number, b: number, c: number, d: number, e: number) => [number, number, number];
+    readonly firmwareCatalogTargetsFromManifest: (a: number, b: number) => [number, number, number];
     readonly ironwingwasmruntime_ackSessionSnapshot: (a: number, b: number, c: number, d: number, e: number) => [number, number, number];
     readonly ironwingwasmruntime_armVehicle: (a: number, b: number) => any;
     readonly ironwingwasmruntime_beginConnect: (a: number) => [number, number, number];
@@ -152,22 +161,12 @@ export interface InitOutput {
     readonly logRawMessagesQuery: (a: number, b: number, c: number, d: number, e: number, f: number, g: any) => [number, number, number];
     readonly logTelemetryAt: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: bigint) => [number, number, number];
     readonly logTelemetryTrack: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number, number];
-    readonly availableMessageRates: () => [number, number, number];
-    readonly start: () => void;
-    readonly webTransportDescriptors: (a: number, b: number, c: number) => [number, number, number];
-    readonly firmwareBootloaderCatalogTargetsFromManifest: (a: number, b: number, c: number, d: number) => [number, number, number];
-    readonly firmwareCatalogEntriesFromManifest: (a: number, b: number, c: number, d: number, e: number) => [number, number, number];
-    readonly firmwareCatalogTargetsFromManifest: (a: number, b: number) => [number, number, number];
     readonly webSerialFirmwareInstallUpdate: (a: number, b: number, c: any, d: any, e: any, f: any, g: any) => any;
-    readonly __wbg_wasmbytebridge_free: (a: number, b: number) => void;
-    readonly wasmbytebridge_close: (a: number) => void;
-    readonly wasmbytebridge_isClosed: (a: number) => number;
-    readonly wasmbytebridge_nextOutbound: (a: number) => any;
-    readonly wasmbytebridge_pushInbound: (a: number, b: any) => any;
-    readonly wasm_bindgen__convert__closures_____invoke__hdb519a66166eae10: (a: number, b: number, c: any) => [number, number];
-    readonly wasm_bindgen__convert__closures_____invoke__h3c099da163a3c89b: (a: number, b: number, c: any, d: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h50282636345e2517: (a: number, b: number) => number;
-    readonly wasm_bindgen__convert__closures_____invoke__h631c439813541c6b: (a: number, b: number) => void;
+    readonly webTransportDescriptors: (a: number, b: number, c: number) => [number, number, number];
+    readonly start: () => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h157532a21e05ac18: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h831f6e81e7b0bf6f: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hba32844d06b6d595: (a: number, b: number) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
     readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
     readonly __wbindgen_exn_store: (a: number) => void;
